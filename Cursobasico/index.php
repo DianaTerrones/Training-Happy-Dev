dc07d5f66e35211e1b68752d78dab259438d9549
DianaTerrones <buddydiana@gmail.com>
2020-10-29 00:54:21 -0600

    PHP
    finish

--- Cursobasico/index.php
+++ Cursobasico/index.php
@@ -140,7 +140,7 @@
 		<div class="container-fluid">
 			<div class="row">
 				<div class="columna columna-41 columna-mobile-100 empujar-58 empujar-mobile-0 sinpadding-mobile">
-					<form action="" method="post">
+					<form action="index.php" method="post">
 						<div class="form-block">
 							<input type="text" name="nombre" class="form-control" placeholder="Nombre">
 						</div>
@@ -153,6 +153,37 @@
 						<div class="form-block bloque-ultimo">
 							<input type="submit" class="boton boton-negro" value="Enviar">
 						</div>
+						<?php
+
+						if($_SERVER ["REQUEST_METHOD"] == "POST")
+						{
+							$nombre = $_POST ["nombre"] ;
+							$email = $_POST ["email"] ;
+							$mensaje = $_POST ["mensaje"] ;
+
+							if(isset($nombre))
+							{
+								if(isset($email))
+								{
+									if(isset($mensaje))
+
+									{
+										$para = "buddydiana@gmail.com";
+										$asunto = "Esto es una prueba";
+										$cuerpo = $nombre."\n".$email."\n".$mensaje;
+										$adicional = "From: noreply@jacoboamaru.com";
+
+										mail($para,$asunto,$cuerpo,$adicional);
+										?>
+										<p>Envio exitoso</p>
+										<?php
+									}
+
+								}
+							}
+						}
+
+						?>
 					</form>
 				</div>
 			</div>
--- Cursobasico/js/funciones.js
+++ Cursobasico/js/funciones.js
@@ -12,14 +12,14 @@ jQuery(".hamb i").toggleClass("fa-times");
 });	
 
 jQuery("header .container nav a").click(function(){
-
+	
 	jQuery("header .container nav").removeClass("open");
 	jQuery(".hamb i").removeClass("fa-times");
 	
 	var dev = jQuery(this).attr("href");
 
 	jQuery("html,body").animate({
-		"scrollTop": jQuery(dev).offset().top
+		"scrollTop": jQuery(dev).offset().top -76
 	})
 })
 }
